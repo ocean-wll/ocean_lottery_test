edae32c974645b12aeebc0eb4690865fe56e9938
ocean_wll <wanglinglong@shulie.io>
2021-09-29 16:25:25 +0800

    引入dubbo的隐式传参

--- src/main/java/pers/ocean/lottery/test/controller/ApiController.java
+++ src/main/java/pers/ocean/lottery/test/controller/ApiController.java
@@ -1,14 +1,13 @@
-package pers.ocean.lottery.test;
+package pers.ocean.lottery.test.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.alibaba.fastjson.JSON;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pers.ocean.lottery.rpc.IActivityBooth;
 import pers.ocean.lottery.rpc.req.ActivityReq;
 import pers.ocean.lottery.rpc.res.ActivityRes;
@@ -16,21 +15,21 @@ import pers.ocean.lottery.rpc.res.ActivityRes;
 /**
  * @Description
  * @Author ocean_wll
- * @Date 2021/8/26 7:33 下午
+ * @Date 2021/9/27 10:26 上午
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApiTest {
+@RestController
+public class ApiController {
 
-    private final Logger logger = LoggerFactory.getLogger(ApiTest.class);
+    private final Logger logger = LoggerFactory.getLogger(ApiController.class);
 
     @Reference(interfaceClass = IActivityBooth.class)
     private IActivityBooth activityBooth;
 
-    @Test
+    @GetMapping("/rpc")
     public void test_rpc() {
         ActivityReq req = new ActivityReq();
         req.setActivityId(100002L);
+        RpcContext.getContext().setAttachment("x-token", "1111");
         ActivityRes result = activityBooth.queryActivityById(req);
         logger.info("测试结果：{}", JSON.toJSONString(result));
     }
--- src/test/java/pers/ocean/lottery/test/DubboGenericServiceTest.java
+++ src/test/java/pers/ocean/lottery/test/DubboGenericServiceTest.java
@@ -1,11 +1,12 @@
 package pers.ocean.lottery.test;
 
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ReferenceConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.bootstrap.DubboBootstrap;
-import org.apache.dubbo.config.utils.ReferenceConfigCache;
-import org.apache.dubbo.rpc.service.GenericService;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.utils.ReferenceConfigCache;
+import com.alibaba.dubbo.rpc.service.GenericService;
+
 import org.junit.Test;
 
 /**
@@ -21,37 +22,37 @@ public class DubboGenericServiceTest {
     }
 
     public void dubboServer() {
-        ApplicationConfig app = new ApplicationConfig("simpleSpring");
-        //RegistryConfig reg = new RegistryConfig("multicast://224.5.6.7:1234");
-        RegistryConfig reg = new RegistryConfig("zookeeper://127.0.0.1:2181");
-
-        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
-        bootstrap.application(app);
-        bootstrap.registry(reg);
-        bootstrap.start();
-
-        try {
-            // 引用远程服务
-            ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
-            // 弱类型接口名
-            reference.setInterface("pers.ocean.samplespring.service.DubboHelloService");
-            //reference.setGroup("dev");
-            //reference.setVersion("1.0");
-            //reference.setRetries(0);
-            //// RpcContext中设置generic=gson
-            //RpcContext.getContext().setAttachment("generic","gson");
-            // 声明为泛化接口
-            reference.setGeneric(true);
-            reference.setCheck(false);
-            GenericService genericService = ReferenceConfigCache.getCache().get(reference);
-            // 传递参数对象的json字符串进行一次调用
-            Object res = genericService.$invoke("sayHi", new String[] {}, new Object[] {});
-            System.err.println(
-                "result[setUser]：" + res); // 响应结果:result[setUser]：{name=Tom, class=com.xxx.api.service.User, age=24}
-        } catch (Throwable ex) {
-            // ignore
-        } finally {
-            bootstrap.stop();
-        }
+        //ApplicationConfig app = new ApplicationConfig("simpleSpring");
+        ////RegistryConfig reg = new RegistryConfig("multicast://224.5.6.7:1234");
+        //RegistryConfig reg = new RegistryConfig("zookeeper://127.0.0.1:2181");
+        //
+        //DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        //bootstrap.application(app);
+        //bootstrap.registry(reg);
+        //bootstrap.start();
+        //
+        //try {
+        //    // 引用远程服务
+        //    ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
+        //    // 弱类型接口名
+        //    reference.setInterface("pers.ocean.samplespring.service.DubboHelloService");
+        //    //reference.setGroup("dev");
+        //    //reference.setVersion("1.0");
+        //    //reference.setRetries(0);
+        //    //// RpcContext中设置generic=gson
+        //    //RpcContext.getContext().setAttachment("generic","gson");
+        //    // 声明为泛化接口
+        //    reference.setGeneric(true);
+        //    reference.setCheck(false);
+        //    GenericService genericService = ReferenceConfigCache.getCache().get(reference);
+        //    // 传递参数对象的json字符串进行一次调用
+        //    Object res = genericService.$invoke("sayHi", new String[] {}, new Object[] {});
+        //    System.err.println(
+        //        "result[setUser]：" + res); // 响应结果:result[setUser]：{name=Tom, class=com.xxx.api.service.User, age=24}
+        //} catch (Throwable ex) {
+        //    // ignore
+        //} finally {
+        //    bootstrap.stop();
+        //}
     }
 }
